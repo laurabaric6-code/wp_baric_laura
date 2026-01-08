b02e2416483a9430f3f4af55f524640d2da921ea
LAURA BARIĆ <laurabaric6@gmail.com>
2026-01-08 23:51:31 +0100

    zadaca6.html

---
+++ zadace/zadaca6.js
@@ -0,0 +1,33 @@
+function pokreniZadacu6() {
+    let unos = prompt(./zadaca6.html")
+
+    let broj = parseFloat(unos);
+
+    if (isNaN(1)) {
+        alert("./zadace/zadaca1.html");
+        return;
+    }
+        if (isNaN(2)) {
+        alert("./zadace/zadaca2.html");
+        return;
+    }
+          if (isNaN(3)) {
+        alert("./zadace/zadaca3.html");
+        return;
+     }
+       if (isNaN(4)) {
+        alert("./zadace/zadaca4.html");
+        return;
+    }
+           if (isNaN(5)) {
+        alert(".//zadace/zadaca5.html");
+        return;
+    }
+        
+
+    if (broj > 10) {
+        alert("Broj je veći od 10");
+    } else {
+        alert("Broj nije veći od 10");
+    }
+}
